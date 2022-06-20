40db5a994b31e5da31a3264eee18e03f0358eb43
Andy <ayeh616@gmail.com>
2022-06-20 14:22:25 -0700

    discrete loop does not work

--- pid.py
+++ pid.py
@@ -13,8 +13,8 @@ class PID(object):
         self.prev_error = 0
         self.prev_time = 0
         self.integral_error = 0
-        self.max_u = 20
-        self.max_windup = 20  # integrator accumulator
+        self.max_u = 1000
+        # self.max_windup = 1000  # integrator accumulator
         # plotting
         self.time_arr = np.array([])
         self.kpe_arr = np.array([])
@@ -34,10 +34,10 @@ class PID(object):
         proportional_error = error
         self.integral_error = (self.integral_error + error) * time_step
         # limit integral windup/accumulation
-        if self.ki * self.integral_error > self.max_windup:
-            self.integral_error = self.max_windup/self.ki
-        elif self.ki * self.integral_error < -self.max_windup:
-            self.integral_error = -self.max_windup/self.ki
+        # if self.ki * self.integral_error > self.max_windup:
+        #     self.integral_error = self.max_windup/self.ki
+        # elif self.ki * self.integral_error < -self.max_windup:
+        #     self.integral_error = -self.max_windup/self.ki
         derivative_error = (error - self.prev_error) / time_step
         u_output = self.kp * proportional_error + self.ki * self.integral_error + self.kd * derivative_error
         # limit u output/signal to actuator
@@ -47,7 +47,7 @@ class PID(object):
             u_output = 0
         self.prev_error = error
         self.prev_time = time
-        print(f'r:{reference}, y:{measured_value}, e:{error}, u:{u_output}')
+        # print(f'r:{reference}, y:{measured_value}, e:{error}, u:{u_output}')
         # plotting
         self.time_arr = np.append(self.time_arr, time)
         self.kpe_arr = np.append(self.kpe_arr, self.kp * proportional_error)
@@ -69,7 +69,7 @@ class PID(object):
         plt.ylabel('position')
         plt.legend()
         plt.subplot(2, 1, 2)
-        plt.plot(self.time_arr, self.u_arr, label='thrust')
+        plt.plot(self.time_arr, self.u_arr, label='u')
         plt.ylabel('u')
         plt.xlabel('time')
         plt.legend()
@@ -90,7 +90,7 @@ class PID(object):
         plt.show()
 
 class MASS_SPRING_DAMPER_SYSTEM(object):
-    def __init__(self, M: float, C: float, K: float):
+    def __init__(self, M: float, K: float, C: float):
         """
         sum_F(t) = F(t) - c*x(t)_dot - k*x(t) = m*x(t)_dotdot
         x(t)_dotdot = -k/m * x(t) -c/m * x(t)_dot + F(t)/m
@@ -98,6 +98,9 @@ class MASS_SPRING_DAMPER_SYSTEM(object):
         self.m = M  # mass constant
         self.c = C  # dampening constant
         self.k = K  # spring constant
+        self.vel = 0
+        self.pos = 0
+        self.prev_time = 0
     
     def state_space(self):
         """
@@ -127,15 +130,28 @@ class MASS_SPRING_DAMPER_SYSTEM(object):
         print(result)
         return result
 
+    def excite(self, time, u):
+        F = u
+        time_step = time - self.prev_time
+        acc = F/self.m - self.c/self.m*self.vel - self.k/self.m*self.pos
+        # acc = -9.8 + u/1
+        self.vel = self.vel + acc*time_step
+        self.pos = self.pos + self.vel*time_step
+        self.prev_time = time
+        return self.pos
+    
+    def get_pos(self):
+        return self.pos
+
 # http://apmonitor.com/pdc/index.php/Main/ModelSimulation
-def model(x_bar: list, t: np.array, u, m, c, k):
+def model(x_bar: list, t: np.array, m, c, k):
     F = REF(t)
     # print(t, F)
-
     x = x_bar[0]
     x_dot = x_bar[1]
     x_dotdot = F/m - c/m*x_dot - k/m*x
-    return [x_dot, x_dotdot]
+    x_bardot = [x_dot, x_dotdot]
+    return x_bardot
 
 def REF(t):
     if t>50:
@@ -145,24 +161,49 @@ def REF(t):
     
 if __name__ == '__main__':
     print('hello')
-    mymodel = MASS_SPRING_DAMPER_SYSTEM(1, 2, 3)
+    mymodel = MASS_SPRING_DAMPER_SYSTEM(1, 20, 10)
     (A, B, C, D) = mymodel.state_space()  
     
     sys = control.ss(A, B, C, D)
-    print(sys)
+    print(f'sys: {sys}')
     sys2 = control.ss2tf(sys)
-    print(sys2)
-    print(control.tf('s'))
+    print(f'P: {sys2}')
+    # print(control.tf('s'))
     
     TIME_STEP = 0.1
     TIME = np.arange(0+TIME_STEP, 100+TIME_STEP, TIME_STEP)
-    # REFERENCE = 1*np.ones(len(TIME))
-    REFERENCE = 1*np.append(np.ones(len(TIME)//2), np.zeros(len(TIME)//2))
+    REFERENCE = 1*np.ones(len(TIME))
+    # REFERENCE = 1*np.append(np.ones(len(TIME)//2), np.zeros(len(TIME)//2))
     
-    t, y = control.input_output_response(sys, TIME, REFERENCE)
-    plt.figure(1)
+    mypid = PID(300, 0, 0)
+    for i in range(len(TIME)):
+        t = TIME[i]
+        r = REFERENCE[i]
+        u = mypid.controller(t, r, mymodel.get_pos())
+        y = mymodel.excite(t, u)
+    mypid.graph()
+    
+    # print(sys2)
+    Kp = 300
+    Ki = 0
+    Kd = 0
+    s = control.tf('s')
+    C = (Kp + Ki/s + Kd*s)
+    print(f'C: {C}')
+    temp = control.series(C, sys2)
+    print(temp)
+    H = control.feedback(temp, 1)
+    print(f'H: {H}, {type(H)}')
+    
+    # print(len(TIME), len(REFERENCE))
+    sys = control.tf2ss(H)
+    t, y = control.forced_response(H, TIME, REFERENCE)
+    print(f'force: {np.shape(y)}')
+    # print(f'force: {np.shape(x)}')
+    plt.figure(2)
     plt.subplot(2, 1, 1)
     plt.plot(t, y)
+    # plt.plot(t, x)
     
     # as the solver has adaptive step size control, that is, 
     # it will use internal time steps that you have no control over, 
@@ -170,13 +211,15 @@ if __name__ == '__main__':
     # Thus there is no connection between the solver time steps and 
     # the data time steps.
     # internal ode time will differ from user selected time
-    y = odeint(model, [0, 0], TIME, args=(REFERENCE, 1, 2, 3))
-    # print(np.shape(y))
-    # print(y)
-    y = y[:, 0]
-    plt.plot(t, y, 'g')
+    y = odeint(model, [0, 0], TIME, args=(1, 10, 20))
+    print(np.shape(y))
+    print(y)
+    y1 = y[:, 0]
+    y2 = y[:, 1]
+    plt.plot(t, y1, 'g')
+    plt.plot(t, y2, 'r')
     
-    t, y = control.step_response(sys, TIME)
+    t, y = control.step_response(H, TIME)
     plt.subplot(2, 1, 2)
     plt.plot(t, y)
     plt.show()
