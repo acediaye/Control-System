c41e18ecb2958f385793009a041975ac577921b5
Andy <ayeh616@gmail.com>
2022-11-13 13:34:40 -0800

    fixed obfb matrix size

--- fsobfb.py
+++ fsobfb.py
@@ -27,7 +27,7 @@ mode = 'obfb'
 if __name__ == '__main__':
     # open loop response
     x0 = np.array([[1],
-                    [-1]])
+                   [-1]])
     tout, yout = control.forced_response(sys, time, ref, x0)
     youtnoise = yout+np.random.normal(0, 0.1, np.shape(time))  # mu, sigma, shape
     if mode == 'ol':
@@ -57,7 +57,7 @@ if __name__ == '__main__':
         dc = control.dcgain(sys_fb)
         K_r = np.array([[1/dc]])
         print(f'ref gain: {K_r}')
-        B_fb = B*K_r
+        B_fb = B@K_r
         sys_fb = control.ss(A_fb, B_fb, C, D)
         print(f'sizes: A_fb:{np.shape(A_fb)} B_fb:{np.shape(B_fb)}, C:{np.shape(C)}, D:{np.shape(D)}')
         x0 = np.array([[1],
@@ -115,24 +115,23 @@ if __name__ == '__main__':
         K = control.place(A, B, poles_c)
         print(f'K: {K}')
         # choosing and placing poles
-        poles_o = [-6+2j, -6-2j]
+        poles_o = [-4+2j, -4-2j]
         L = control.place(A.T, C.T, poles_o).T
         print(f'L: {L}')
         # full state observer feedback
         A_ce = np.bmat([[A-B@K, B@K],
                         [np.zeros(np.shape(A)), A-L@C]])
-        B_temp = np.bmat([[B]],
-                         [[np.zeros(np.shape(B))]])
+        B_temp = np.bmat([[B],
+                         [np.zeros(np.shape(B))]])
         C_ce = np.bmat([[C, np.zeros(np.shape(C))]])
         D_ce = np.zeros(np.shape(D))
-        print(np.shape(A_ce), np.shape(B_temp), np.shape(B))
         # find reference gain
         sys_temp = control.ss(A_ce, B_temp, C_ce, D_ce)
         dc = control.dcgain(sys_temp)
         K_r = np.array([[1/dc]])
         print(f'ref gain: {K_r}')
-        B_ce = np.bmat([[B*K_r],
-                        [np.zeros(np.shape(B))]])  # <-- fix not correct shape
+        B_ce = np.bmat([[B@K_r],
+                        [np.zeros(np.shape(B))]])
         print(f'sizes: A_ce:{np.shape(A_ce)} B_ce:{np.shape(B_ce)}, C_ce:{np.shape(C_ce)}, D_ce:{np.shape(D_ce)}')
         sys_ce = control.ss(A_ce, B_ce, C_ce, D_ce)
         x0 = np.array([[1],
